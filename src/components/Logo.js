e0138f239e07f0fef972a60875053b48cc0126b6
rohitpim1 <pimrohit33@gmail.com>
2024-04-26 14:47:37 +0530

    logo change

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Logo = () => {
   return (
     <div className='logo'>
-        <div className='storeName'><span>D</span>ream<span>V</span>icta</div>
+        <div className='storeName'><span>D</span>ream<span>D</span>victa</div>
         <p className='storeDesc'>SPORTS STORE</p>
     </div>
   )
